eccadadfb444107d4e3c68ac048f012cd3920bae
Yiding Liu <yliu224@dons.usfca.edu>
2018-04-19 13:36:52 -0700

    Compile Error

--- knn.py
+++ knn.py
@@ -13,16 +13,22 @@ class knn(classifier):
 		self.Y = Y
 
 	def predict(self,X):
-		print(self.X)
-		# result = []
-		# for i in range(0,len(self.X)):
-		# 	distance = 0
-		# 	for j in range(0,len(X[i])):
-		# 		print(type(self.X[i][j]))
-		# 		X1 = int(self.X[i][j])
-		# 		X2 = int(X[j])
-		# 		print(type(X1))
-		# 		distance += np.power(X1-X2)
-		# 	result.append([np.sqrt(distance),self.Y[i]])
-		# return result
+		result = []
+		for i in range(0,len(self.X)):
+			distance = 0
+			for j in range(0,len(X)):
+				X1 = int(self.X[i][j])
+				X2 = int(X[j])
+				distance += np.power(X1-X2,2)
+			result.append([np.sqrt(distance),self.Y[i]])
+		result.sort();
+        classify = [0,0,0]
+        for i in range(0,len(result)):
+            if(result[i]==-1):
+                classify[0]+=1
+            if(result[i]==0):
+                classify[1]+=1
+            if(result[i]==1):
+                classify[2]+=1
+        return np.max(classify)
 
